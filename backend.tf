2dc9970dce1537ea92b858286cc84f1e98e3f5d6
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-08 23:18:39 +0530

    Update S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket2"
+    bucket         = "my-ews-baket1"
     region         = "us-east-1"
     key            = "DevOps-Project-11/terraform.tfstate"
     dynamodb_table = "Lock-Files"
