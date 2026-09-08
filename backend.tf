9fc28478299a89051ead3887b22e92ab3f48b01f
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-08 23:18:50 +0530

    Change encryption setting to false in backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     region         = "us-east-1"
     key            = "DevOps-Project-11/terraform.tfstate"
     dynamodb_table = "Lock-Files"
-    encrypt        = true
+    encrypt        = false
   }
   required_version = ">= 1.0.0"
   required_providers {
