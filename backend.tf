5d1570eb625037c6ca6ab35504cd68c88ba5367a
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-12 22:26:10 +0530

    Fix typo in backend.tf key configuration

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
-    key            = "DevOps-Project-11/terraform.tfstate"
+    yeh            = "DevOps-Project-11/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
