7b47df19cfdb54015b375515ea21946e2b5421af
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-20 22:47:10 +0530

    Fix syntax for required_providers in backend.tf

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
     encrypt        = true
   }
   required_version = ">= 1.0.0"
-  required_euue {
+  required_providers {
     aws = {
       version = ">= 5.0.0"
       source  = "hashicorp/aws"
