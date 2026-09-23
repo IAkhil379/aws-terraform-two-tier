55e0164b34bd88c20a12489079640874d5a59423
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-23 22:10:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    toool         = "my-ews-bucket1"
+    bucket         = "my-ews-bucket1"
     region         = "us-east-1"
     key            = "DevOps-Project-11/terraform.tfstate"
     dynamodb_table = "Lock-Files"
