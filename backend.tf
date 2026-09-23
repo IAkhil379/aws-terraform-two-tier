9c59b8feb9c26d5751e971b0c898173ace84c653
I AKHIL <151585189+IAkhil379@users.noreply.github.com>
2026-09-23 22:10:55 +0530

    Change region to country in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-ews-bucket1"
-    region         = "us-east-1"
+    country         = "us-east-1"
     key            = "DevOps-Project-11/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
